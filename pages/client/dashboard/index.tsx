3dbd9e1f7311c652b10d0c8b5a7e955ecd5328c6
hridyacp <jithridya@gmail.com>
2022-12-03 22:47:31 +0530

    Add ens

--- pages/client/dashboard/index.tsx
+++ pages/client/dashboard/index.tsx
@@ -1,94 +1,305 @@
 import { FormControl, InputLabel, MenuItem, OutlinedInput, Select, SelectChangeEvent, Theme, useTheme } from "@mui/material";
-import React, {  useState } from "react";
+import React, { useState, useEffect } from "react";
 import SearchIcon from "@mui/icons-material/Search";
-import nft from "../../../src/json/nft.json";
-import ClientDashboardStyles from './clientdashboard.module.scss';
+import clientABI from "../../../src/abi/client-abi.json";
+import ClientDashboardStyles from "./clientdashboard.module.scss";
 import { useRouter } from "next/router";
+import { ethers } from "ethers";
+import axios from "axios";
+import Web3 from "web3";
+import { Chat } from "@pushprotocol/uiweb";
+import tokenABI from "../../../src/abi/token-abi.json";
+import { AnyARecord } from "dns";
 
 export default function ClientDashboard() {
-      const router = useRouter();
-      function getStyles(name: string, personName: string[], theme: Theme) {
-        return {
-          fontWeight:
-            personName.indexOf(name) === -1
-              ? theme.typography.fontWeightRegular
-              : theme.typography.fontWeightMedium,
-        };
+  const router = useRouter();
+  const [username, setUserName] = useState<string>("");
+  const [avatars, setAvatar] = useState<string>("");
+  const [useraccount, setUserAccount] = useState<string>("");
+  const [enableChat, setenableChat] = useState<boolean>(false);
+  const [approvEn, setapprovEn] = useState<boolean>(false);
+  const [rejectEn, setrejectEn] = useState<boolean>(false);
+  function getStyles(name: string, personName: string[], theme: Theme) {
+    return {
+      fontWeight: personName.indexOf(name) === -1 ? theme.typography.fontWeightRegular : theme.typography.fontWeightMedium,
+    };
+  }
+  const theme = useTheme();
+  const [personName, setPersonName] = useState<string[]>([]);
+  const web3 = new Web3(
+    Web3.givenProvider || "https://quaint-evocative-morning.matic-testnet.discover.quiknode.pro/51f4850ccaec8ad41a57f0dcd7d0e51b694fd6bf/"
+  );
+  const nftaddress = "0x30ede7289d52412c22b78741e78BB153A4EF6b07";
+  const tokenaddress = "0x54fc29154Dc49D36d58A18A8fD035182EB3BB3a1";
+  const [projectdetails, setProjectDetails] = useState([
+    {
+      projectName: "",
+      project_cost: 0,
+      description: "",
+      freelancer: "",
+      id: "",
+      sold: false,
+    },
+  ]);
+  const handleclick = (type: string) => {
+    if (type === "createproject") {
+      router.push("/client/dashboard/createproject");
+    } else if (type === "projects") {
+      router.push("/client/dashboard");
+    }
+  };
+  const handleChange = (event: SelectChangeEvent<typeof personName>) => {
+    const {
+      target: { value },
+    } = event;
+    setPersonName(
+      // On autofill we get a stringified value.
+      typeof value === "string" ? value.split(",") : value
+    );
+  };
+  const getNameAddress = async () => {
+    const provider = new ethers.providers.JsonRpcProvider("https://goerli.infura.io/v3/");
+    let address = localStorage.getItem("walletaddress");
+    let names = await provider.lookupAddress(address!);
+    if (names) {
+      let avatarimage = await provider.getAvatar(names!);
+      setAvatar(avatarimage!);
+    }
+    setUserName(names! ?? address);
+  };
+  const getProjects = async () => {
+    const accounts = await web3.eth.getAccounts();
+    setUserAccount(accounts[0]);
+    const projects = await axios.post("https://api.thegraph.com/subgraphs/name/darahask/nftfree", {
+      query: `{
+        listings(where: {client: "${accounts[0]}"}) {
+          project_cost
+          id
+          freelancer
+          sold
+        }
+      }`,
+    });
+    let totalprojects = projects.data.data.listings;
+    const nftcontract = await new web3.eth.Contract(clientABI as [], nftaddress);
+    for (let i = 0; i < totalprojects.length; i++) {
+      let data = await nftcontract.methods.tokenURI(totalprojects[i].id).call();
+      if (data != "" && data.includes("ipfs")) {
+        const dats = data.split(":/").join("");
+        console.log(dats, i, "data");
+        const nftData = `https://nftstorage.link/${dats}`;
+        const response = await fetch(nftData);
+        const json = await response.json();
+        totalprojects[i].projectName = json.projectName;
+        totalprojects[i].description = json.description;
+      } else {
+        totalprojects[i].projectName = "New project";
+        totalprojects[i].description = "Project on NFT";
       }
-      const theme = useTheme();
-      const [personName, setPersonName] = useState<string[]>([]);
-      const handleclick=(type:string)=>{
-        if(type==="createproject"){
-        router.push('/client/dashboard/createproject')
-        }
-        else if(type==="projects"){
-          router.push('/client/dashboard')
-        }
-        }
-      const handleChange = (event: SelectChangeEvent<typeof personName>) => {
-        const {
-          target: { value },
-        } = event;
-        setPersonName(
-          // On autofill we get a stringified value.
-          typeof value === 'string' ? value.split(',') : value,
-        );
-      };
-    return (
-        <>
-   <div className="justify-center flex h-full w-full">
-      <div className=" bg-[#171717] w-[90%] justify-center flex-col pt-10">
-      <div className="flex gap-44 pl-8 pb-10 pr-10"> 
-        <div className={ClientDashboardStyles.mainhead+ " text-white "}>
-        ARMOR
-        </div>
-        <div className={ClientDashboardStyles.mainheads+ " text-white "}>
-        Dashboard
-        </div>
-        </div>
-        <div className=" flex ">
-        <div className=" min-w-[350px] h-full pl-2 pt-0 ">
-        <div className={" bg-[#171717] pt-0 w-[50%]"}>
-        <div className={" p-2 flex flex-col h-[100vh] gap-8"}>
-            <div className={ClientDashboardStyles.sidemenu+ " p-2 "}>
-              <p className="text-white mt-2.5 pl-2"><button onClick={()=>handleclick("projects")}>PROJECTS</button></p>
+    }
+    setProjectDetails(totalprojects);
+  };
+  const approveBuy = async (id: any, freelanceradd: any, price: any) => {
+    const accounts = await web3.eth.getAccounts();
+    console.log(price);
+    const tokencontract = await new web3.eth.Contract(tokenABI as [], tokenaddress);
+    let tx: any = {
+      from: accounts[0],
+      to: tokenaddress,
+      data: tokencontract.methods.approve(nftaddress, price + 1).encodeABI(),
+    };
+    await web3.eth
+      .sendTransaction({
+        from: accounts[0],
+        to: tokenaddress,
+        data: tx.data,
+        gasPrice: 50000000000,
+      })
+      .then(async function (receipt: any) {
+        const nftcontract = await new web3.eth.Contract(clientABI as [], nftaddress);
+        console.log(id, freelanceradd, "sdfsdfsdf");
+        let tx1: any = {
+          from: accounts[0],
+          to: nftaddress,
+          data: nftcontract.methods.assignProject(id, freelanceradd).encodeABI(),
+        };
+
+        await web3.eth
+          .sendTransaction({
+            from: accounts[0],
+            to: nftaddress,
+            data: tx1.data,
+            gasPrice: 50000000000,
+          })
+          .then(function (receipt: any) {
+            setapprovEn(true);
+            router.push("/client/dashboard");
+          });
+      });
+  };
+  const rejectBuy = async (id: any, freelanceradd: any) => {
+    const accounts = await web3.eth.getAccounts();
+    const nftcontract = await new web3.eth.Contract(clientABI as [], nftaddress);
+    let tx: any = {
+      from: accounts[0],
+      to: nftaddress,
+      data: nftcontract.methods.rejectProject(id, freelanceradd).encodeABI(),
+    };
+
+    await web3.eth
+      .sendTransaction({
+        from: accounts[0],
+        to: nftaddress,
+        data: tx.data,
+        gasPrice: 50000000000,
+      })
+      .then(function (receipt: any) {
+        router.push("/client/dashboard");
+      });
+  };
+  const completeBuy = async (id: any, freelanceradd: any) => {
+    const accounts = await web3.eth.getAccounts();
+    const nftcontract = await new web3.eth.Contract(clientABI as [], nftaddress);
+    let tx: any = {
+      from: accounts[0],
+      to: nftaddress,
+      data: nftcontract.methods.completeProject(id).encodeABI(),
+    };
+
+    await web3.eth
+      .sendTransaction({
+        from: accounts[0],
+        to: nftaddress,
+        data: tx.data,
+        gasPrice: 50000000000,
+      })
+      .then(function (receipt: any) {
+        router.push("/client/dashboard");
+      });
+  };
+  useEffect(() => {
+    getProjects();
+    getNameAddress();
+  }, [approvEn]);
+  return (
+    <>
+      <div className="justify-center flex h-full w-full">
+        <div className=" bg-[#171717] w-[90%] justify-center flex-col pt-10">
+          <div className={ClientDashboardStyles.allhead + " flex gap-4 justify-start"}>
+            <div className={" text-white "}>
+              <img width={"150px"} height="150px" src="/augmnt.png"></img>
             </div>
-            <div className={ClientDashboardStyles.sidemenu+ " p-2 "}>
-              <p className="text-white mt-2.5 pl-2"><button onClick={()=>handleclick("createproject")}>CREATE PROJECT</button></p>
+            <div className={ClientDashboardStyles.mainhead + " text-white "}>AUGMNT</div>
+
+            <div className={ClientDashboardStyles.mainheads + " text-white l-10"}>Dashboard</div>
+
+            <div className={"ml-auto"}>
+              {avatars !== "" ? <img src={avatars}></img> : <img width={"100px"} height="100px" src="/avatarimage.png"></img>}
             </div>
+            <div className={ClientDashboardStyles.mainname + " text-white text-md ml-auto"}>{username}</div>
+          </div>
+          <div className=" flex ">
+            <div className=" min-w-[350px] h-full pl-2 pt-0 ">
+              <div className={" bg-[#171717] pt-0 w-[50%]"}>
+                <div className={" p-2 flex flex-col h-[100vh] gap-8"}>
+                  <div className={ClientDashboardStyles.sidemenu + " p-2 "}>
+                    <p className="text-white mt-2.5 pl-2">
+                      <button onClick={() => handleclick("projects")}>PROJECTS</button>
+                    </p>
+                  </div>
+                  <div className={ClientDashboardStyles.sidemenu + " p-2 "}>
+                    <p className="text-white mt-2.5 pl-2">
+                      <button onClick={() => handleclick("createproject")}>CREATE PROJECT</button>
+                    </p>
+                  </div>
                 </div>
+              </div>
+            </div>
+            <div className="md:w-[95%] overflow-hidden gap-4 flex-col bg-[#2E2E2E] p-10">
+              <div className={ClientDashboardStyles.sidehead + " p-8 uppercase"}>Available Projects</div>
+              <div className={ClientDashboardStyles.projectlist}>
+                <div className={" md:w-[95%] flex flex-col overflow-hidden gap-4 text-white p-4 b-1"}>
+                  <div className={ClientDashboardStyles.nftlist + " flex gap-4 justify-between"}>
+                    <span>PROJECT NAME</span>
+                    <span>PROJECT BUDGET</span>
+                    <span>PROJECT STATUS</span>
+                    <span>ACTIONS</span>
+                  </div>
+                  {projectdetails[0].projectName !== "" &&
+                    projectdetails?.map((item, index) => (
+                      <div className={ClientDashboardStyles.nftlist + " flex gap-4 justify-between"}>
+                        <div>{item.projectName}</div>
+                        <div>{item.project_cost}</div>
+                        <div>{item.sold ? "On going" : "Created"}</div>
+                        <div>
+                          <button
+                            className={ClientDashboardStyles.bidbtns}
+                            key={item.id}
+                            onClick={() => {
+                              console.log("qqqqqqqq", item.freelancer, item.sold);
+                              setenableChat(true);
+                            }}
+                            disabled={item.sold ? false : true}
+                          >
+                            Chat
+                          </button>
+                          {!approvEn && (
+                            <div className=" flex justify-end w-full">
+                              <button
+                                className={ClientDashboardStyles.bidbtnn}
+                                key={item.id}
+                                onClick={() => {
+                                  console.log("qqqqqqqq", item.freelancer, item.sold);
+                                  approveBuy(item.id, item.freelancer, item.project_cost);
+                                }}
+                                disabled={item.sold ? false : true}
+                              >
+                                Approve
+                              </button>
+                              <button
+                                className={ClientDashboardStyles.bidbtn}
+                                key={item.id}
+                                onClick={() => {
+                                  console.log("qqqqqqqq", item.freelancer, item.sold);
+                                  rejectBuy(item.id, item.freelancer);
+                                }}
+                                disabled={item.sold ? false : true}
+                              >
+                                Reject
+                              </button>
+                            </div>
+                          )}
+                          {approvEn && (
+                            <div>
+                              <button
+                                className={ClientDashboardStyles.bidbtn}
+                                key={item.id}
+                                onClick={() => {
+                                  completeBuy(item.id, item.freelancer);
+                                }}
+                                disabled={item.sold ? false : true}
+                              >
+                                Complete
+                              </button>
+                            </div>
+                          )}
+                          {enableChat && (
+                            <Chat
+                              account={useraccount} //user address
+                              supportAddress={item.freelancer} //support address
+                              apiKey="WaPzABQ56k.aGFbWHdl5RVMbkwqoenzgfALVwWTQVotWFhkFO6qwpaX5PCQm7sM8NQaC0pgpLNX"
+                              env="staging"
+                            />
+                          )}
+                        </div>
+                      </div>
+                    ))}
                 </div>
-                </div>
-                <div className="md:w-[95%] overflow-hidden gap-4 flex-col bg-[#2E2E2E] p-10">
-    <div className={ClientDashboardStyles.sidehead+ " p-8 uppercase"}>
-Available Projects
-    </div>
-    <div className={ClientDashboardStyles.projectlist}>
-     <div className={" md:w-[95%] flex flex-col overflow-hidden gap-4 text-white p-4 b-1"}>
-     <div className={ClientDashboardStyles.nftlist + " flex gap-4 justify-between"}>
-      <span>COMPANY NAME</span>
-      <span>PROJECT BUDGET</span>
-      <span>PROJECT STATUS</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-        {nft.map((item,index)=>( 
-       <div className={ClientDashboardStyles.nftlist + " flex gap-4 justify-between"}>
-        <div>
-        {item["company name"]}
-        </div>
-        <div>
-        {item.budget}
-        </div>
-        <div>
-        {item.email}
-        </div>
-        </div>
-        ))}
-        </div>
-        </div>
-        </div>
-        </div>
-        </div>
-    </div>
-        </>
-    )
+    </>
+  );
 }
--- pages/signup/clientsignup/index.tsx
+++ pages/signup/clientsignup/index.tsx
@@ -1,7 +1,8 @@
-import ClientSignupstyles from './clientsignup.module.scss'
+import ClientSignupstyles from "./clientsignup.module.scss";
 import { useRouter } from "next/router";
-import React, {  useState,useRef,useEffect } from "react";
-
+import React, { useState, useRef, useEffect } from "react";
+import Web3 from "web3";
+import clientABI from "../../../src/abi/client-abi.json";
 export default function ClientSignup() {
   const router = useRouter();
   const [emailErr, setEmailErr] = useState<boolean>(false);
@@ -14,118 +15,140 @@ export default function ClientSignup() {
     contact: "",
     email: "",
   });
-
+  const web3 = new Web3(
+    Web3.givenProvider || "https://quaint-evocative-morning.matic-testnet.discover.quiknode.pro/51f4850ccaec8ad41a57f0dcd7d0e51b694fd6bf/"
+  );
+  const nftaddress = "0x30ede7289d52412c22b78741e78BB153A4EF6b07";
   const assignClasses = (emailval: string) => {
-    if (
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailval)
-    ) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailval)) {
       setEmailErr(false);
     } else {
       setEmailErr(true);
     }
   };
   const assignClassesContact = (contactRef: string) => {
-    if (
-      /^[0-9]{10}$/.test(
-        (contactRef)
-      )
-    ) {
+    if (/^[0-9]{10}$/.test(contactRef)) {
       setcontactErr(false);
     } else {
       setcontactErr(true);
     }
   };
-  const handleInputChange = (
-    event: React.ChangeEvent<HTMLInputElement>,
-    inputType: string
-  ) => {
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>, inputType: string) => {
     let updatedFields: any = {};
-    updatedFields["name"] =
-    inputType === "name" ? event.target.value : fields.name;
-    updatedFields["company"] =
-    inputType === "company" ? event.target.value : fields.company;
-    updatedFields["contact"] =
-    inputType === "contact" ? event.target.value : fields.contact;
-  updatedFields["email"] =
-    inputType === "email" ? event.target.value : fields.email;
+    updatedFields["name"] = inputType === "name" ? event.target.value : fields.name;
+    updatedFields["company"] = inputType === "company" ? event.target.value : fields.company;
+    updatedFields["contact"] = inputType === "contact" ? event.target.value : fields.contact;
+    updatedFields["email"] = inputType === "email" ? event.target.value : fields.email;
     setFields(updatedFields);
   };
-  const createClient=()=>{
-    console.log(fields)
-    if(fields.company ==='')
-    setcompanyErr(true);
-    if(fields.name ==='')
-   setnameErr(true);
-    if(fields.contact ==='')
-    setcontactErr(true);
-     if (fields.email ==='') 
-    setEmailErr(true);
-     if(fields.contact!==''){
-     assignClassesContact(fields.contact);
+  const createClient = async () => {
+    if (fields.company === "") setcompanyErr(true);
+    if (fields.name === "") setnameErr(true);
+    if (fields.contact === "") setcontactErr(true);
+    if (fields.email === "") setEmailErr(true);
+    if (fields.contact !== "") {
+      if (/^[0-9]{10}$/.test(fields.contact)) {
+        setcontactErr(false);
+      } else {
+        setcontactErr(true);
+      }
     }
-     if(fields.email!==''){
-     assignClasses(fields.email);
-   }
-   if(fields.company!==''){
-     setcompanyErr(false);
-   }
-   if(fields.name !==''){
-    setnameErr(false);
-   }
-    else{
-
+    if (fields.email !== "") {
+      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(fields.email)) {
+        setEmailErr(false);
+      } else {
+        setEmailErr(true);
+      }
     }
- }
- useEffect(() => {
- }, [fields]);
+    if (fields.company !== "") {
+      setcompanyErr(false);
+    }
+    if (fields.name !== "") {
+      setnameErr(false);
+    }
+    if (!emailErr && !contactErr && !companyErr && !nameErr) {
+      console.log(fields);
+      const nftcontract = await new web3.eth.Contract(clientABI as [], nftaddress);
+      web3.eth.getAccounts().then((address: any) => {
+        let tx: any = {
+          from: address[0],
+          to: nftaddress,
+          data: nftcontract.methods.addClient(fields.name, fields.email, fields.contact, fields.company).encodeABI(),
+        };
+        console.log(address[0], "accounttttttttttt");
+        web3.eth
+          .sendTransaction({
+            from: address[0],
+            to: nftaddress,
+            data: tx.data,
+          })
+          .then(function (receipt: any) {
+            router.push("/client/dashboard");
+          });
+      });
+    }
+  };
+  useEffect(() => {}, [fields]);
   return (
     <>
       <div>
-      <div className={ClientSignupstyles.limiter+" h-full"}>
-		<div className={ClientSignupstyles.containerlogin100}>
-			<div className={ClientSignupstyles.wraplogin100+" p-t-30 p-b-50"}>
-				<span className={ClientSignupstyles.login100formtitle+" p-b-41 p-6"}>
-					WELCOME TO ARMOR
-				</span>
-				<div className={ClientSignupstyles.login100form + " "}>
-        <div className={ClientSignupstyles.logintext +" flex flex-col justify-center items-center font-header uppercase "}>
-        <span>Contact details</span>
-        </div>
-					<div className={ " flex flex-col gap-1 p-6 justify-center"}>
-          <div  className="text-center justify-center p-4">
-						<input defaultValue={fields.name} className={ClientSignupstyles.input100} onChange={(e) => handleInputChange(e, "name")} placeholder="Name"/>
-                        {nameErr &&
-  <span className="text-red-600 text-md">Please add name name</span>}
+        <div className={ClientSignupstyles.limiter + " h-full"}>
+          <div className={ClientSignupstyles.containerlogin100}>
+            <div className={ClientSignupstyles.wraplogin100 + " p-t-30 p-b-50"}>
+              <span className={ClientSignupstyles.login100formtitle + " p-b-41 p-6"}>WELCOME TO AUGMNT</span>
+              <div className={ClientSignupstyles.login100form + " "}>
+                <div className={ClientSignupstyles.logintext + " flex flex-col justify-center items-center font-header uppercase "}>
+                  <span>Contact details</span>
+                </div>
+                <div className={" flex flex-col gap-1 p-6 justify-center"}>
+                  <div className="text-center justify-center p-4">
+                    <input
+                      defaultValue={fields.name}
+                      className={ClientSignupstyles.input100}
+                      onChange={(e) => handleInputChange(e, "name")}
+                      placeholder="Name"
+                    />
+                    {nameErr && <span className="text-red-600 text-md">Please add name name</span>}
+                  </div>
+                  <div className="text-center justify-center p-4">
+                    <input
+                      defaultValue={fields.company}
+                      className={ClientSignupstyles.input100}
+                      onChange={(e) => handleInputChange(e, "company")}
+                      placeholder="Company Name"
+                    />
+                    {companyErr && <span className="text-red-600 text-md">Please add company name</span>}
+                  </div>
+                  <div className="text-center justify-center p-4">
+                    <input
+                      defaultValue={fields.email}
+                      className={ClientSignupstyles.input100}
+                      onChange={(e) => handleInputChange(e, "email")}
+                      placeholder="Email"
+                    />
+                    {emailErr && <span className="text-red-600 text-md">Please add Email Id</span>}
+                  </div>
+                  <div className="text-center justify-center p-4">
+                    <input
+                      defaultValue={fields.contact}
+                      className={ClientSignupstyles.input100}
+                      onChange={(e) => handleInputChange(e, "contact")}
+                      placeholder="Contact"
+                    />
+                    {contactErr && <span className="text-red-600 text-md">Please add contact number</span>}
+                  </div>
+                </div>
+
+                <div className={ClientSignupstyles.containerlogin100formbtn + " p-4"}>
+                  <button className={ClientSignupstyles.login100formbtn} onClick={() => createClient()}>
+                    Sign Up
+                  </button>
+                </div>
+              </div>
             </div>
-            <div  className="text-center justify-center p-4">
-          <input defaultValue={fields.company} className={ClientSignupstyles.input100} onChange={(e) => handleInputChange(e, "company")}placeholder="Company Name" />
-          {companyErr &&
-  <span className="text-red-600 text-md">Please add company name</span>}
           </div>
-          <div  className="text-center justify-center p-4">
-          <input defaultValue={fields.email} className={ClientSignupstyles.input100} onChange={(e) => handleInputChange(e, "email")} placeholder="Email" />
-          {emailErr &&
-  <span className="text-red-600 text-md">Please add Email Id</span>}
-          </div>
-          <div className="text-center justify-center p-4">
-          <input defaultValue={fields.contact} className={ClientSignupstyles.input100} onChange={(e) => handleInputChange(e, "contact")} placeholder="Contact" />
-          {contactErr &&
-  <span className="text-red-600 text-md">Please add contact number</span>}
-          </div>
-					</div>
-
-					<div className={ClientSignupstyles.containerlogin100formbtn+" p-4"}>
-                    <button className={ClientSignupstyles.login100formbtn} onClick={()=>createClient()}>
-              Sign Up
-            </button>
-					</div>
-
-				</div>
-			</div>
-		</div>
-	</div>
- 
-  </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
